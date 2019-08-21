5022e0b5cf864b6cbe3180537e7366d8be1d5f6a
tina <tina@transposit.com>
2019-08-21 14:24:27 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -7,24 +7,8 @@
  */
 ({ http_event }) => {
   const parsed_body = http_event.parsed_body;
-  const workspaceId = parsed_body.team_id;
-  const userId = parsed_body.user_id;
-  const response_url = parsed_body.response_url;
 
-  setImmediate(() => {
-    let user = api.user({type: "slack", workspaceId, userId});
-    if (user) {
-      let message = api.run('this.get_slack_message', {}, {asUser: user.id})[0];
-      api.run("slack_webhook.post_to_response_url", {
-        response_url: response_url,
-        post_body: message
-      });      
-    } else {
-      api.run("slack_webhook.post_to_response_url", {
-        response_url: response_url,
-        post_body: {text: 'Please configure your user at ' + env.getBuiltin().appUrl}
-      });      
-    }
-  });
+  console.log(http_event);
+
   return { status_code: 200 };
 }
