5459de9d49170257de7158654947dc7ffd2d99a9
tina <tina@transposit.com>
2019-08-21 14:21:32 +0000

    Commit from Transposit IDE

--- src/test_update_submodule.js
+++ src/test_update_submodule.js
@@ -2,16 +2,19 @@
   let owner = env.get('parentOwner');
   let repo = env.get('parentRepo');
   let branch = env.get('parentBranch');
-  
   let submoduleOwner = env.get('submoduleOwner');
   let submoduleRepo = env.get('submoduleRepo');
-  let submoduleBranch = env.get('submoduleBranch');
-  
+  let submoduleBranch = env.get('submoduleBranch');  
   let submodulePath = env.get('submodulePath');
     
   let parentSha = api.run('github.get_branch_for_repo', {owner, repo, branch})[0].commit.sha;
   let submoduleSha = api.run('github.get_branch_for_repo', {owner: submoduleOwner, repo: submoduleRepo, branch: submoduleBranch})[0].commit.sha;
 
+  let oldSubmoduleSha = api.run('github.get_files_in_repo', {owner: owner, repo: repo, ref: branch, path: submodulePath})[0].sha;  
+  if (oldSubmoduleSha === submoduleSha) {
+    return 'No update needed';
+  }
+  
   let treeSha = api.run('github.create_git_tree', {
     owner: owner,
     repo: repo,
